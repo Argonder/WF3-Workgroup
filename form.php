5ffe9ff3ca1e8cc653287bdc131481beebc3d841
Leslie <leleu.leslie@outlook.com>
2017-11-28 14:30:53 +0100

    form1
    formulaire html

---
+++ form.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		Formulaire
+	</title>
+</head>
+<body>
+
+<form>
+	<label> Pseudo </label>
+	<input type="text" name="pseudo">
+	<label> Mot de passe </label>
+	<input type="password" name="password">
+</form>
+
+</body>
+</html>
