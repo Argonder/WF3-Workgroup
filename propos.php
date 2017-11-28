07ab72bc2f4cdbd67e26ec44df3878e42acfd8f8
Jeylo <ydervaux@msn.com>
2017-11-28 15:18:33 +0100

    Ajout A propos
    Ajout a propos

---
+++ propos.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>A propos</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+</head>
+<body>
+
+<nav class="navbar navbar-default">
+
+        <div class="container">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#"> LOGO </a>
+            </div>
+            <div class="collapse navbar-collapse" id="menu">
+            <ul class="nav navbar-nav navbar-right">
+            <li> <a href="index.html" > Accueil </a></li>
+            <li> <a href="form.php" > Contact </a></li>
+            <li> <a href="propos.php" > A propos </a></li>
+            </ul>
+            </div>
+        </div>
+    </nav>
+
+<div class="col-md-3">
+	<h2>Lorem Ipsum</h2>
+	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et hendrerit dolor. Aliquam pulvinar magna sit amet sodales interdum. Fusce rhoncus nunc nulla, quis maximus magna sodales eget. Donec fringilla, nibh sit amet convallis fermentum, orci eros volutpat odio, in fringilla leo lectus nec metus. Mauris lacinia, felis ac semper gravida, tellus nisi finibus libero, sit amet ullamcorper ex nisi sit amet mi. In hac habitasse platea dictumst. Donec placerat, nisi non porta suscipit, enim libero mollis orci, eu luctus dolor sem ac enim. Maecenas scelerisque venenatis ex nec iaculis. Etiam in purus id eros ultricies auctor eu nec nisl. Pellentesque aliquam nunc quis nisl fringilla convallis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla rutrum ante dolor, id fermentum lectus cursus nec. Integer laoreet maximus arcu, non luctus velit vulputate in. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. </p>
+	
+</div>
+<img src="./img/chouette.jpg">
+</body>
+</html>
